024ca09c8a5c414382f3efddddaae020b1cb4661
Haykhovhannisyan1 <hayk.9816@gmail.com>
2025-07-29 17:25:57 +0400

    fix token

--- chains/solana/token/tests/anchor-htlc.ts
+++ chains/solana/token/tests/anchor-htlc.ts
@@ -1,4 +1,4 @@
-import assert from "assert";
+// import assert from "assert";
 import * as anchor from "@coral-xyz/anchor";
 import { randomBytes, createHash } from "crypto";
 import * as spl from '@solana/spl-token';
